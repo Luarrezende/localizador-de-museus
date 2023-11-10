4d59189e1c2c336a9791668c8a631da49e2a0adf
Arthur Kieling <luarrezende@gmail.com>
2023-11-09 22:46:02 -0300

    feat: resolve requisito 2 e tenta resolver requisito 3

---
+++ src/main/java/com/betrybe/museumfinder/exception/InvalidCoordinateException.java
@@ -0,0 +1,8 @@
+package com.betrybe.museumfinder.exception;
+
+/**
+ * Exception to invalid coordinate.
+ */
+public class InvalidCoordinateException extends RuntimeException {
+  public InvalidCoordinateException() {}
+}
---
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -0,0 +1,34 @@
+package com.betrybe.museumfinder.service;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.util.CoordinateUtil;
+import org.springframework.stereotype.Service;
+
+/**
+ * Exception to invalid coordinate.
+ */
+@Service
+public class MuseumService implements MuseumServiceInterface {
+  MuseumFakeDatabase museumFakeDatabase;
+
+  public MuseumService(MuseumFakeDatabase museumFakeDatabase) {
+    this.museumFakeDatabase = museumFakeDatabase;
+  }
+
+  @Override
+  public Museum createMuseum(Museum museum) {
+    double latitude = museum.getCoordinate().latitude();
+    double longitude = museum.getCoordinate().longitude();
+
+    Coordinate coordinate = new Coordinate(latitude, longitude);
+
+    if (!CoordinateUtil.isCoordinateValid(coordinate)) {
+      throw new InvalidCoordinateException();
+    }
+
+    return museumFakeDatabase.saveMuseum(museum);
+  }
+}
