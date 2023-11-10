90cf60f7ede17ae810c4e02220a58d195b1302b8
Arthur Kieling <luarrezende@gmail.com>
2023-11-09 23:34:13 -0300

    feat: resolve requisito 4

--- src/main/java/com/betrybe/museumfinder/service/MuseumService.java
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -2,9 +2,11 @@ package com.betrybe.museumfinder.service;
 
 import com.betrybe.museumfinder.database.MuseumFakeDatabase;
 import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.exception.MuseumNotFoundException;
 import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.util.CoordinateUtil;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 /**
@@ -23,11 +25,34 @@ public class MuseumService implements MuseumServiceInterface {
   }
 
   @Override
+  public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
+    Optional<Museum> closeM = museumFakeDatabase.getClosestMuseum(coordinate, maxDistance);
+    if (!checkCoordinate(coordinate)) {
+      throw new InvalidCoordinateException();
+    }
+
+    if (closeM.isEmpty()) {
+      throw new MuseumNotFoundException();
+    }
+
+    return closeM.get();
+  }
+
+  @Override
   public Museum createMuseum(Museum museum) {
     if (!checkCoordinate(museum.getCoordinate())) {
       throw new InvalidCoordinateException();
     }
 
     return museumFakeDatabase.saveMuseum(museum);
+  }
+
+  @Override
+  public Museum getMuseum(Long id) {
+    Optional<Museum> museum = museumFakeDatabase.getMuseum(id);
+    if (museum.isEmpty()) {
+      throw new MuseumNotFoundException();
+    }
+    return museum.get();
   }
 }
